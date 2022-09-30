e5edf1d26363bc931c8cedc89d16cc62021bb483
OscarVargas09 <omvargast@ufpso.edu.co>
2022-09-29 21:03:48 -0500

    Se realizan validaciones y codigos de estado Http

--- src/main/java/com/procesos/negocio/oscar/controllers/UsuarioController.java
+++ src/main/java/com/procesos/negocio/oscar/controllers/UsuarioController.java
@@ -3,6 +3,8 @@ package com.procesos.negocio.oscar.controllers;
 import com.procesos.negocio.oscar.models.Usuario;
 import com.procesos.negocio.oscar.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,63 +17,90 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     @GetMapping(value = "/usuario/{id}")
-    public Optional<Usuario> getUsuario(@PathVariable Long id){
+    public ResponseEntity getUsuario(@PathVariable Long id){
        Optional<Usuario> usuario= usuarioRepository.findById(id);
-       return usuario;
+       if(usuario.isPresent()){
+           return new ResponseEntity(usuario, HttpStatus.OK);
+       }
+       return ResponseEntity.notFound().build();
 
     }
 
     @PostMapping("/usuario")
-    public Usuario crearUsuario(@RequestBody Usuario usuario){
-        usuarioRepository.save(usuario);
-        return usuario;
+    public ResponseEntity crearUsuario(@RequestBody Usuario usuario){
+        try{
+            usuarioRepository.save(usuario);
+            return new ResponseEntity(usuario, HttpStatus.CREATED);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 
-    @GetMapping(value = "/all")
-    public List<Usuario> listarUsuarios(){
-        return usuarioRepository.findAll();
+    @GetMapping(value = "/usuarios")
+    public ResponseEntity listarUsuarios(){
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        if(usuarios.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(usuarios, HttpStatus.OK);
     }
 
     @GetMapping("/usuario/{nombre}/{apellidos}")
-    public List<Usuario> listarPorNombreApellidos(@PathVariable String nombre,@PathVariable String apellidos){
-        return usuarioRepository.findAllByNombreAndApellidos(nombre, apellidos);
+    public ResponseEntity listarPorNombreApellidos(@PathVariable String nombre,@PathVariable String apellidos){
+
+        List<Usuario> usuarios = usuarioRepository.findAllByNombreAndApellidos(nombre, apellidos);
+        if(usuarios.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(usuarios,HttpStatus.OK);
     }
 
     @GetMapping("/usuario/nombre/{nombre}")
-    public List<Usuario> listarPorNombre(@PathVariable String nombre){
-        return usuarioRepository.findAllByNombre(nombre);
+    public ResponseEntity listarPorNombre(@PathVariable String nombre){
+        List<Usuario> usuarios = usuarioRepository.findAllByNombre(nombre);
+        if(usuarios.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(usuarios,HttpStatus.OK);
     }
 
     @GetMapping("/usuario/apellido/{apellidos}")
-    public List<Usuario> listarPorApellido(@PathVariable String apellidos){
-        return usuarioRepository.findAllByApellidos(apellidos);
+    public ResponseEntity listarPorApellido(@PathVariable String apellidos){
+        List<Usuario> usuarios = usuarioRepository.findAllByApellidos(apellidos);
+        if(usuarios.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity(usuarios,HttpStatus.OK);
     }
 
     @PutMapping("/usuario/{id}")
-    public Usuario editarUsuario(@PathVariable Long id,@RequestBody Usuario usuario){
-        Usuario usuarioBd= usuarioRepository.findById(id).get();
-        try {
-            usuarioBd.setNombre(usuario.getNombre());
-            usuarioBd.setApellidos(usuario.getApellidos());
-            usuarioBd.setDireccion(usuario.getDireccion());
-            usuarioBd.setDocumento(usuario.getDocumento());
-            usuarioBd.setFechaNacimiento(usuario.getFechaNacimiento());
-            usuarioBd.setTelefono(usuario.getTelefono());
-            usuarioRepository.save(usuarioBd);
-            return usuarioBd;
-        }catch (Exception e){
-            return null;
+    public ResponseEntity editarUsuario(@PathVariable Long id,@RequestBody Usuario usuario){
+        Optional<Usuario> usuarioBd= usuarioRepository.findById(id);
+        if(usuarioBd.isPresent()){
+            try {
+                usuarioBd.get().setNombre(usuario.getNombre());
+                usuarioBd.get().setApellidos(usuario.getApellidos());
+                usuarioBd.get().setDireccion(usuario.getDireccion());
+                usuarioBd.get().setDocumento(usuario.getDocumento());
+                usuarioBd.get().setFechaNacimiento(usuario.getFechaNacimiento());
+                usuarioBd.get().setTelefono(usuario.getTelefono());
+                usuarioRepository.save(usuarioBd.get());
+                return new ResponseEntity(usuarioBd, HttpStatus.OK);
+            }catch (Exception e){
+                return ResponseEntity.badRequest().build();
+            }
         }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/usuario/{id}")
-    public Usuario eliminarUsuario(@PathVariable Long id){
-        Usuario usuarioBd = usuarioRepository.findById(id).get();
-        try {
-            usuarioRepository.delete(usuarioBd);
-            return usuarioBd;
-        }catch (Exception e){
-            return null;
+    public ResponseEntity eliminarUsuario(@PathVariable Long id){
+        Optional<Usuario> usuarioBd = usuarioRepository.findById(id);
+        if(usuarioBd.isPresent()){
+            usuarioRepository.delete(usuarioBd.get());
+            return ResponseEntity.noContent().build();
         }
+        return ResponseEntity.notFound().build();
     }
 }
